d4bf9e83e783a216e4249cae3aab65932b8e16b6
Gloria Jeyech <gjeyech@gmail.com>
2024-10-03 10:56:21 +0300

    update

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+    elif length == 1:
+        print("[0]")
+        return
+    
+    fib_sequence = [0, 1]
+    for i in range(2, length):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    
+    print(f"[{', '.join(map(str, fib_sequence[:length]))}]")
